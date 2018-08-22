be97df6917c579daae327be601aa458e808be9b4
Phil Hopper <philhopper@sibertec.com>
2018-08-22 08:18:21 -0400

    Start nginx also

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,9 @@ thisDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${thisDir}"
 source venv/bin/activate
 
+# start nginx
+nginx -g "daemon off;" &
+
 # start the flask app with uwsgi
 cd "${thisDir}/public"
 uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi:app --ini ../config/obs-pdf.ini
