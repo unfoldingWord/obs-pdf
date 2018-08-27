94c92a44edf82cd123377f66e15f525771f9c7c4
Phil Hopper <philhopper@sibertec.com>
2018-08-27 07:44:00 -0400

    Debugging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,4 +11,4 @@ nginx -g "daemon off;" &
 
 # start the flask app with uwsgi
 cd "${thisDir}/public"
-uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi:app --ini ../config/obs-pdf.ini
+#uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi:app --ini ../config/obs-pdf.ini
