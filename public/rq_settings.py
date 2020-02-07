122d8e4721e51bdbeb4304378b1603e063e3f740
Robert Hunt <Freely.Given.org@gmail.com>
2020-02-07 14:28:43 +1300

    Update queue name (now have 3 tX queues)

--- public/rq_settings.py
+++ public/rq_settings.py
@@ -1,4 +1,4 @@
-# TX PDF RQ Settings
+# TX OBS PDF RQ Settings
 
 from os import getenv
 
@@ -14,7 +14,7 @@ REDIS_URL = getenv('REDIS_URL', 'redis://127.0.0.1:6379')
 
 # Queues to listen on
 #QUEUES = ['high', 'normal', 'low'] # NOTE: The first queue in the list is processed first
-ENQUEUE_NAME = 'tX_PDF_webhook' # Becomes the queue name -- MUST match tx_enqueue_main.py in tx-enqueue-job
+ENQUEUE_NAME = 'tX_OBS_PDF_webhook' # Becomes the queue name -- MUST match tx_enqueue_main.py in tx-enqueue-job
 prefix = getenv('QUEUE_PREFIX', '') # Gets (optional) QUEUE_PREFIX environment variable -- set to 'dev-' for development
 QUEUE_NAME_SUFFIX = '' # Used to switch to a different queue, e.g., '_1'
 webhook_queue_name = prefix + ENQUEUE_NAME + QUEUE_NAME_SUFFIX
